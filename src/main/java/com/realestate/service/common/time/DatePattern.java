ca44405eecc240814e94f59b1bbe5f788713d29c
박한빈 <phb0228@naver.com>
2022-02-13 14:46:20 +0900

    공통 날짜 패턴 클래스를 추가하라

---
+++ src/main/java/com/realestate/service/common/time/DatePattern.java
@@ -0,0 +1,11 @@
+package com.realestate.service.common.time;
+
+/**
+ * 공통 날짜 패턴을 관리 합니다.
+ */
+public class DatePattern {
+  /**
+   * 기본 날짜 패턴.
+   */
+  public static final String DEFAULT_DATE = "yyyy-MM-dd";
+}
