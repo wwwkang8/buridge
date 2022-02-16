88d173d0eacab7cf476e14ebf8599f1795809ffd
phb0228 <phb0228@naver.com>
2022-02-16 16:42:24 +0900

    계약 타입 enum을 추가하라

---
+++ src/main/java/com/realestate/service/property/constant/ContractType.java
@@ -0,0 +1,20 @@
+package com.realestate.service.property.constant;
+
+/**
+ * 계약 타입을 관리합니다.
+ */
+public enum ContractType {
+  /**
+   * 매매.
+   */
+  SALE,
+  /**
+   * 전세.
+   */
+  LARGE_DEPOSIT,
+  /**
+   * 월세.
+   */
+  MONTHLY_LEASE,
+  ;
+}
