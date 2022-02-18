cbdb154ce4f517ab2210a36c7135e7375d013588
phb0228 <phb0228@naver.com>
2022-02-18 18:50:08 +0900

    매물 repository에 조회 메서드를 추가하라

--- src/main/java/com/realestate/service/property/address/entity/PropertyAddressRepository.java
+++ src/main/java/com/realestate/service/property/address/entity/PropertyAddressRepository.java
@@ -1,8 +1,13 @@
 package com.realestate.service.property.address.entity;
 
+import java.util.Optional;
+
+import com.realestate.service.property.entity.Property;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PropertyAddressRepository extends JpaRepository<PropertyAddress, Long> {
+
+  Optional<PropertyAddress> findByProperty(Property property);
 }
--- src/main/java/com/realestate/service/property/entity/PropertyRepository.java
+++ src/main/java/com/realestate/service/property/entity/PropertyRepository.java
@@ -1,8 +1,18 @@
 package com.realestate.service.property.entity;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PropertyRepository extends JpaRepository<Property, Long> {
+
+  @Query("select p from Property p "
+       + "where p.id = :id "
+       + "and p.user.id = :userId "
+       + "and p.deletedDateTime is null ")
+  Optional<Property> findByIdAndUserId(@Param("id") long id, @Param("userId") long userId);
 }
