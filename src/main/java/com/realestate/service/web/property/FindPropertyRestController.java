31ddaa83357aef795d79a9dd4cc0e4a653e98cf6
박한빈 <phb0228@naver.com>
2022-03-20 13:51:17 +0900

    매물 목록 조회 엔드포인트를 작성하라

---
+++ src/main/java/com/realestate/service/web/property/FindPropertyRestController.java
@@ -0,0 +1,23 @@
+package com.realestate.service.web.property;
+
+import javax.validation.Valid;
+
+import com.realestate.service.property.FindPropertyUseCase;
+import com.realestate.service.property.entity.PropertyDataResponse;
+import com.realestate.service.web.property.request.FindPropertyRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Slice;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class FindPropertyRestController {
+
+  private final FindPropertyUseCase findPropertyUseCase;
+
+  @GetMapping("/api/properties")
+  public Slice<PropertyDataResponse> find(@Valid FindPropertyRequest request) {
+    return findPropertyUseCase.find(request.toCommand());
+  }
+}
