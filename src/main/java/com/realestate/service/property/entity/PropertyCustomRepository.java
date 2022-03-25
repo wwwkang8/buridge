5190412b1f44b7f5433343273fce1dae64c79f24
박한빈 <phb0228@naver.com>
2022-03-25 20:07:03 +0900

    매물 상세 조회 쿼리를 추가하라

--- src/main/java/com/realestate/service/property/entity/PropertyCustomRepository.java
+++ src/main/java/com/realestate/service/property/entity/PropertyCustomRepository.java
@@ -1,13 +1,23 @@
 package com.realestate.service.property.entity;
 
+import java.util.Optional;
+
 import com.realestate.service.property.dto.FindPropertyQueryDto;
 import org.springframework.data.domain.Slice;
 
 public interface PropertyCustomRepository {
   /**
+   * 매물 번호에 맞는 매물 정보를 반환합니다.
+   *
+   * @param id 매물 고유 번호
+   * @return   매물 상세 정보
+   */
+  Optional<PropertyDetailDataResponse> find(long id);
+
+  /**
    * 요청에 맞는 매물 정보를 반환합니다.
    *
-   * @param queryDto 매물 조회 요청 dto.
+   * @param queryDto 매물 조회 요청 dto
    */
   Slice<PropertyDataResponse> find(FindPropertyQueryDto queryDto);
 }
